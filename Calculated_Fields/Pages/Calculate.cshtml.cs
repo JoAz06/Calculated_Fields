061444ccbdaf70d22173a83b3c7676ec580243a4
Joseph <Josephazzi2006@gmail.com>
2026-01-07 16:01:42 +0200

    Data doesnt have to be static, rename not working

--- Calculated_Fields/Pages/Calculate.cshtml.cs
+++ Calculated_Fields/Pages/Calculate.cshtml.cs
@@ -1,8 +1,11 @@
+using Antlr.Runtime.Collections;
 using Calculated_Fields.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using NCalc;
+using System.Collections;
+using System.Diagnostics.Eventing.Reader;
 
 namespace Calculated_Fields.Pages{
     
@@ -10,87 +13,69 @@ namespace Calculated_Fields.Pages{
         private readonly Calculated_Fields.Data.CalculatedTextFieldContext _context;
         public CalculateModel(Calculated_Fields.Data.CalculatedTextFieldContext context)
         {
-            _context = context; 
+            _context = context;
+            AllFields ??= new List<TextField>();
+            Results ??= new Dictionary<int, string>();
+            GettingRenamed ??= new HashSet<int>();
         }
+
         [BindProperty(SupportsGet = true)]
-        public TextField text1 { get; set; }
-        [BindProperty]
-        public bool ShowRename1 { get; set; } = false;
-        [BindProperty(SupportsGet = true)]
-        public TextField text2 { get; set; }
-        [BindProperty]
-        public bool ShowRename2 { get; set; } = false;
-        [BindProperty(SupportsGet = true)]
-        public TextField text3 { get; set; }
-        [BindProperty]
-        public bool ShowRename3 { get; set; } = false;
-        public string result;
-        public async Task<IActionResult> OnGetAsync()
-        {
-            text1 = _context.TextField.FirstOrDefault(x => x.Id == 1);
-            text2 = _context.TextField.FirstOrDefault(x => x.Id == 2);
-            text3 = _context.TextField.FirstOrDefault(x => x.Id == 3);
-            var expression = new Expression(text3.value);
-            expression.Parameters[text1.name] = double.Parse(text1.value);
-            expression.Parameters[text2.name] = double.Parse(text2.value);
-            try
-            {
-                result = expression.Evaluate().ToString();
+        public List<TextField> AllFields { get; set; }
+        public Dictionary<int,string> Results { get; set; }
+        public HashSet<int> GettingRenamed { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(){
+            AllFields = await _context.TextField.ToListAsync();
+            foreach (TextField field in AllFields) {
+                if (field.type.Equals(Calculated_Fields.Models.Type.CALCULATED)) {
+                    Calculater(field);
+                }
             }
-            catch
-            {
-                result = "Error in calculation";
-            }
-            ShowRename1 = false;
-            ShowRename2 = false;
-            ShowRename3 = false;
             return Page();
         }   
 
         public async Task<IActionResult> OnPostCalculateAsync() {
-            _context.TextField.FirstOrDefault(x => x.Id == 1).value = text1.value;
-            _context.TextField.FirstOrDefault(x => x.Id == 2).value = text2.value;
-            _context.TextField.FirstOrDefault(x => x.Id == 3).value = text3.value;
-            _context.TextField.FirstOrDefault(x => x.Id == 1).name = text1.name;
-            _context.TextField.FirstOrDefault(x => x.Id == 2).name = text2.name;
-            _context.TextField.FirstOrDefault(x => x.Id == 3).name = text3.name;
+            GettingRenamed ??= new HashSet<int>();
+            foreach (TextField field in AllFields) {
+                var toBeUpdated = _context.TextField.Find(field.Id);
+                if (toBeUpdated != null) {
+                    toBeUpdated.name = field.name;
+                    toBeUpdated.value = field.value;
+                    if (toBeUpdated.type.Equals(Calculated_Fields.Models.Type.CALCULATED)) {
+                        Calculater(toBeUpdated);
+                    }
+                }
+            }
             _context.SaveChanges();
-            text1 = _context.TextField.FirstOrDefault(x => x.Id == 1);
-            text2 = _context.TextField.FirstOrDefault(x => x.Id == 2);
-            text3 = _context.TextField.FirstOrDefault(x => x.Id == 3);
-            var expression = new Expression(text3.value);
-            
-            expression.Parameters[text1.name] = double.Parse(text1.value);
-            expression.Parameters[text2.name] = double.Parse(text2.value);
-            try { 
-            result = expression.Evaluate().ToString();
+            AllFields = await _context.TextField.ToListAsync();
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostRenameAsync(int Id) {
+            GettingRenamed.Add(Id);
+            Console.WriteLine(GettingRenamed.Contains(1));
+            return RedirectToPage();
+        }
+
+        public async Task<IActionResult> OnPostAddField() {
+            return Page();
+        }
+
+        public void Calculater(TextField toBeUpdated) {
+            var expression = new Expression(toBeUpdated.value);
+            foreach (TextField field2 in AllFields) {
+                if (field2.Id != toBeUpdated.Id)
+                    expression.Parameters[field2.name] = double.Parse(field2.value);
             }
-            catch
-            {
-                result = "Error in calculation";
+            try {
+                Results[toBeUpdated.Id] = expression.Evaluate().ToString();
             }
-            ShowRename1 = false;
-            ShowRename2 = false;
-            ShowRename3 = false;
-            return Page();
-        }
-
-        public async Task<IActionResult> OnPostRename1Async()
-        {
-            ShowRename1= true;
-            return Page();
-        }
-
-        public async Task<IActionResult> OnPostRename2Async()
-        {
-            ShowRename2 = true;
-            return Page();
-        }
-
-        public async Task<IActionResult> OnPostRename3Async()
-        {
-            ShowRename3 = true;
-            return Page();
+            catch (NCalc.EvaluationException) {
+                Results[toBeUpdated.Id] = "You used invalid syntax."; 
+            }
+            catch {
+                Results[toBeUpdated.Id] = "An unkown error has occurred while calculating.";
+            }
         }
     }
     
